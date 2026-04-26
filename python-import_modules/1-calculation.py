2cbd67a7b8a46cb84ebafd62dbd72a9da6ea4d38
Okelechi <k.oparaji@alustudent.com>
2026-04-25 23:50:51 -0700

    added a new file

---
+++ python-import_modules/1-calculation.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+from calculator import add, sub
+if __name__ == "__main__":
+    a = 10
+    b = 5
+    print("{} + {} = {}".format a, b, (add)))
+    print("{} - {} = {}".format a, b, (sub)))
