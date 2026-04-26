2ba6ad33c5ebf2e8113b0512d752faca6cc1b008
Okelechi <k.oparaji@alustudent.com>
2026-04-25 23:54:23 -0700

    added a new file

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
-from calculator import add, sub
+from calculator_1 import add, sub, mul, div
+
 if __name__ == "__main__":
     a = 10
     b = 5
-    print("{} + {} = {}".format a, b, (add)))
-    print("{} - {} = {}".format a, b, (sub)))
+
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
